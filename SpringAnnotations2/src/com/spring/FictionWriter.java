3ece8cb5bab39277ae4179a4ca384abed95e0f34
ajith <ajithaj941@gmail.com>
2024-03-25 16:04:43 +0530

    Spring Annotation2

---
+++ SpringAnnotations2/src/com/spring/FictionWriter.java
@@ -0,0 +1,18 @@
+package com.spring;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class FictionWriter implements Award{
+
+	@Override
+	public void award() {
+		// TODO Auto-generated method stub
+		System.out.println("Fiction Writing award");
+		
+	}
+	
+	
+
+}
---
+++ SpringAnnotations2/src/com/spring/Test.java
@@ -0,0 +1,19 @@
+package com.spring;
+
+import org.springframework.beans.factory.BeanFactory;  
+import org.springframework.beans.factory.xml.XmlBeanFactory;  
+import org.springframework.core.io.ClassPathResource;  
+import org.springframework.core.io.Resource; 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+  
+public class Test {  
+public static void main(String[] args) {  
+   
+	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+	Writer obj = (Writer)context.getBean("writer");
+	obj.getAward();
+	
+	
+	context.close();
+}  
+}  
