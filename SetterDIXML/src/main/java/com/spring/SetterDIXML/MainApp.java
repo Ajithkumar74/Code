a87757401fd70158e2fcf1921be3c97355834537
ajith <ajithaj941@gmail.com>
2024-03-25 16:02:49 +0530

    SetterDIXML

---
+++ SetterDIXML/src/main/java/com/spring/SetterDIXML/MainApp.java
@@ -0,0 +1,22 @@
+package com.spring.SetterDIXML;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class MainApp {
+	public static void main( String[] args )
+    {
+        // Create the application context using the configuration class
+
+    	AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(AppConfig.class);
+    	//Retrieve the beans from the context
+    	BeanA beanA=context.getBean(BeanA.class);
+    	BeanB beanB=context.getBean(BeanB.class);
+    	BeanC beanC=context.getBean(BeanC.class);
+        // You can now use the beans as needed
+    	 beanA.performOperationA();
+         beanB.performOperationB();
+         beanC.performOperationC();
+    }
+
+}
